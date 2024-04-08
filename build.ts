fad4181c420987008bba30072956367b8e073a90
Flow <highflowey@gmail.com>
2024-04-08 14:29:04 +0330

    made a directory for lune executables

--- build.ts
+++ build.ts
@@ -1,15 +1,45 @@
 import path from "path";
 import * as config from "./lune-config.json";
 import * as child_process from "child_process";
-import { readFileSync, writeFile } from "fs";
+import { readFileSync, unlinkSync, writeFile } from "fs";
 
 const luneConfigPath = "./lune-config.json";
-let regex = /Writing standalone binary to (.+)/;
+const regex = /Writing standalone binary to (.+)/;
+const extensionRegex = /(\..+)/;
+
 __dirname = path.resolve(__dirname, "../");
 
-let build = child_process.spawn("lune", ["build", config.mainScript], {
-  cwd: __dirname,
+let buildSample = child_process.spawnSync(
+  "lune",
+  ["build", config.mainScript],
+  {
+    cwd: __dirname,
+  }
+);
+
+let extension = "";
+
+buildSample.output.forEach((chunk) => {
+  if (!chunk) return;
+  let stdout = chunk.toString();
+  let output = stdout.match(regex);
+
+  if (output) {
+    let sampleExecutable = output[1];
+    let extensionRegexResults = output[1].match(extensionRegex);
+    extension = extensionRegexResults ? extensionRegexResults[1] : "";
+
+    unlinkSync(sampleExecutable);
+  }
 });
+
+let build = child_process.spawn(
+  "lune",
+  ["build", config.mainScript, "-o", "./executables/build" + extension],
+  {
+    cwd: __dirname,
+  }
+);
 
 build.stdout.on("data", (chunk: Buffer) => {
   let stdout = chunk.toString();
